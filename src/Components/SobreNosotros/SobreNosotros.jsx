5fe4c1414d21cba86f0abcae486931b95d036f73
aninajns <valenciacamilo5926@gmail.com>
2025-01-22 11:00:42 -0500

    Finalizacion de la seccion sobre nosotros

--- src/Components/SobreNosotros/SobreNosotros.jsx
+++ src/Components/SobreNosotros/SobreNosotros.jsx
@@ -2,18 +2,18 @@ import './SobreNosotros.css'
 
 export const SobreNosotros = () => {
   return (
-    <div id='SobreNosotros'>
+    <section id='SobreNosotros'>
       <h2>Sobre Nosotros</h2>
       <div id='tarjetas'>
         <img src="../../../public/fotoSobreNosotros.jfif" alt="" />
         <div id='tarjetaPrincipal'>
           <h3>Restaurante Ensigna</h3>
-          <h2>BARRIGA LLENA CORAZON CONTENTO</h2>
+          <h2>Sabor que encanta, frescura que se siente.</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, a dolores architecto, deleniti excepturi quos ea aperiam tempore, quibusdam ab id mollitia omnis inventore sunt magni soluta maiores rem voluptatum!</p>
           <button>LEER MAS</button>
         </div>
         <img src="../../../public/hotoSobreNosotros2.jfif" alt="" />
       </div>
-    </div>
+    </section>
   )
 }
