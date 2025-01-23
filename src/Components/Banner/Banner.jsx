e5757180f512adcadf7714f6fe95f6a561599141
aninajns <valenciacamilo5926@gmail.com>
2025-01-23 12:43:57 -0500

    .

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ export const Banner = () => {
       <div id='banner'>
         <video src="../../../public/videoBanner.mp4" autoPlay muted loop ></video>
         <div id='ContenidoBanner'>
-          <h1>Bienvenido a Restaurante Ensigna</h1>
+          <h1>Bienvenido Restaurante Ensigna</h1>
           <p>Donde nuestra comida es deliciosa y unica</p>
           <div id='botones'>
             <Button texto={'VER CARTA'} colorTexto={'white'} colorFondo={'black'}/>
